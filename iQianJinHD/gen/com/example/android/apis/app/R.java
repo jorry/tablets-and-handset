fe33c2673793e649765e9f224e017022d1af3d76
jorryliu <156224301@qq.com>
2014-08-27 18:57:50 +0800

    JOY

--- iQianJinHD/gen/com/example/android/apis/app/R.java
+++ iQianJinHD/gen/com/example/android/apis/app/R.java
@@ -34,26 +34,28 @@ public final class R {
         public static final int shadow_left=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f090009;
+        public static final int action_settings=0x7f09000a;
         public static final int button1=0x7f090002;
         public static final int details=0x7f090001;
-        public static final int groupmain=0x7f090003;
-        public static final int seekBar1=0x7f090007;
-        public static final int sildingFinishLayout=0x7f090008;
-        public static final int tab2=0x7f090004;
-        public static final int textView1=0x7f090006;
+        public static final int five=0x7f090004;
+        public static final int groupmain=0x7f090005;
+        public static final int seekBar1=0x7f090009;
+        public static final int sildingFinishLayout=0x7f090003;
+        public static final int tab2=0x7f090006;
+        public static final int textView1=0x7f090008;
         public static final int titles=0x7f090000;
-        public static final int viewFlipper1=0x7f090005;
+        public static final int viewFlipper1=0x7f090007;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int base=0x7f030001;
         public static final int details=0x7f030002;
-        public static final int group=0x7f030003;
-        public static final int item=0x7f030004;
-        public static final int list=0x7f030005;
-        public static final int listotodetails=0x7f030006;
-        public static final int two=0x7f030007;
+        public static final int five=0x7f030003;
+        public static final int group=0x7f030004;
+        public static final int item=0x7f030005;
+        public static final int list=0x7f030006;
+        public static final int listotodetails=0x7f030007;
+        public static final int two=0x7f030008;
     }
     public static final class menu {
         public static final int main=0x7f080000;
--- iQianJinHD/src/com/example/android/apis/app/FiveFragment.java
+++ iQianJinHD/src/com/example/android/apis/app/FiveFragment.java
@@ -1,33 +1,32 @@
 package com.example.android.apis.app;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.Button;
 
-@SuppressLint("NewApi")
-public class OtherFragment extends Fragment{
+public class FiveFragment extends Fragment{
 
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
-		View v = inflater.inflate(R.layout.two, null);
-		Button b  = (Button)v.findViewById(R.id.button1);
-		b.setOnClickListener(new OnClickListener() {
+		View v = inflater.inflate(R.layout.five, null);
+		v.findViewById(R.id.five).setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View arg0) {
-//				OtherFragment.this.getFragmentManager().popBackStack();
-				getFragmentManager().beginTransaction().replace(R.id.details,new FiveFragment(), "other").commit();
+				// TODO Auto-generated method stub
+			getChildFragmentManager().popBackStack("ljw", 0);
 			}
 		});
 		return v;
 	}
-	
-	
 }
